ff41fd7c838c7ec852bbffb7984d43d38bb7c8aa
shanwebfix <shanwebfix@gmail.com>
2025-06-21 20:02:19 +0600

    updtae

--- script.js
+++ script.js
@@ -16,6 +16,19 @@ closeMenu.addEventListener('click', () => {
 
 
 
+// header white scroll class js
+  const header = document.querySelector('.main-header');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+
+
+
 
 
 
